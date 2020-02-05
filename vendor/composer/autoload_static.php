3189dc98db1460bc8590b9a333eeb37cc7cdc7fb
DouglasBA <douglas_b_azevedo@hotmail.com>
2020-02-04 21:40:14 -0300

    autoload hcodebr classes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInite037ff69f982fd91a49b56f3c79d8ab4
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInite037ff69f982fd91a49b56f3c79d8ab4
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInite037ff69f982fd91a49b56f3c79d8ab4::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInite037ff69f982fd91a49b56f3c79d8ab4::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInite037ff69f982fd91a49b56f3c79d8ab4::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInite037ff69f982fd91a49b56f3c79d8ab4::$prefixesPsr0;
             $loader->classMap = ComposerStaticInite037ff69f982fd91a49b56f3c79d8ab4::$classMap;
 
