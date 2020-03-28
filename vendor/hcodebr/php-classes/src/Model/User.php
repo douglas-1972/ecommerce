9da1e534fdc06ed4b8541b3956ae36199877b47f
DouglasBA <douglas_b_azevedo@hotmail.com>
2020-03-28 11:39:19 -0300

    Minha Conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -13,6 +13,7 @@ class User extends Model {
 	const SECRET_IV = "HcodePhp7_Secret";
 	const ERROR = "UserError";
 	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "UserSucess";
 
 	public static function getFromSession()
 	{
@@ -300,6 +301,32 @@ class User extends Model {
 
 		$_SESSION[User::ERROR] = NULL;
 	}
+
+	//sucess
+	public static function setSuccess($msg)
+	{
+
+		$_SESSION[User::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess()
+	{
+
+		$msg = (isset($_SESSION[User::SUCCESS])) ? $_SESSION[User::SUCCESS] : "";
+		
+		User::clearSuccess();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess()
+	{
+
+		$_SESSION[User::SUCCESS] = NULL;
+	}
+	//
 
 
 	public static function setErrorRegister($msg)
